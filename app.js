02aa8f31c9c3bade41e099e77842cc6026017f64
Apple Artic <techapple652@gmail.com>
2022-11-21 22:53:40 +0530

    completed project

--- app.js
+++ app.js
@@ -2,27 +2,17 @@ const container = document.getElementById('container');
 container.style.border = "2px #2171ff solid";
 container.style.display = "flex";
 container.style.width = `640px`;
-
-function reset(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            let square = document.getElementById(`${i}-${j}`);
-            square.style.backgroundColor = "white";
-        }
-    }
-}
-
-function myFunc1(gridSize) {
-    const arr = [];//An array of divs
-
+let arr = [];//An array of divs
+function makeGrid(gridSize=16) {
     const boxSize = (640 - (gridSize * 4)) / gridSize;
+    arr = [];
     for (let i = 0; i < gridSize; i++) {
         const x = document.createElement('div');
         for (let j = 0; j < gridSize; j++) {
             const square = document.createElement('div');
             square.style.width = `${boxSize}px`;
             square.style.height = `${boxSize}px`;
-            square.style.background = "white";
+            square.style.backgroundColor = "white";
             square.style.margin = "0";
             square.style.padding = "0";
             square.style.border = "2px #212121 solid";
@@ -34,7 +24,25 @@ function myFunc1(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         container.appendChild(arr[i]);
     }
+}
 
+function reset(gridSize=16) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const element = document.getElementById(`${j}-${i}`);
+            element.style.backgroundColor = "white";
+        }
+    }
+}
+
+function myFunc1(gridSize=16) {
+    console.log(container.childElementCount);
+    if(container.childElementCount>0){
+        while(container.firstChild){
+            container.removeChild(container.lastChild);
+        }
+    }
+    makeGrid(gridSize);
     function randomColorSelection() {
         const randomColor = Math.trunc(Math.random() * 256);
         return randomColor;
@@ -50,21 +58,21 @@ function myFunc1(gridSize) {
     container.addEventListener('mouseover', colorDiv);
 }
 
-let cnt = 0;
-
 function myFunc2(e) {
     let event = e.target.id;
     let gridSize = document.getElementById('grid-size').value;
+    if (gridSize === "") {
+        gridSize = 16;
+    }
     if (gridSize < 16 || gridSize > 100) {
         alert("Grid Size should be from 16 to 100");
-    } else {
+    } else if (gridSize >= 16 && gridSize <= 100) {
         if (event === 'submit') {
-            reset(gridSize);
             myFunc1(gridSize);
         } else if (event === 'reset') {
             reset(gridSize);
         }
     }
 }
-const submit=document.getElementById('submit');
-submit.addEventListener('click', myFunc2);
+
+window.addEventListener('click', myFunc2);
